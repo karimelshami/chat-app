9a9dc9cde17be639d1ab7e227841aa75eb9f429b
Karim El Shami <karim.elshami@vezeeta.com>
2019-11-12 13:56:06 +0200

    Updates

---
+++ precache-manifest.4d522ad8a61d48fd314b1af8139f9a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a16594f482f1b4a5ff29ce6720e099d",
+    "url": "/karimelshami/chat-app/index.html"
+  },
+  {
+    "revision": "0364362caa1b3f3fba85",
+    "url": "/karimelshami/chat-app/static/js/2.d1d87144.chunk.js"
+  },
+  {
+    "revision": "0de2bb929336ec99dc35",
+    "url": "/karimelshami/chat-app/static/js/main.eb114e50.chunk.js"
+  },
+  {
+    "revision": "1329130377107fd8fb8f",
+    "url": "/karimelshami/chat-app/static/js/runtime-main.cf709478.js"
+  },
+  {
+    "revision": "139cf12c53a980a14ed1a5e9d37c3ed6",
+    "url": "/karimelshami/chat-app/static/media/bunq.139cf12c.svg"
+  },
+  {
+    "revision": "5005dbccb59bc9929274c043b848eb84",
+    "url": "/karimelshami/chat-app/static/media/lock.5005dbcc.gif"
+  }
+]);
